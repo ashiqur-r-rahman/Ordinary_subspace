0c2257acb35211a269dbc81e6ae0f8f2a0ebb35e
ashiqur-r-rahman <shohag36725@gmail.com>
2025-10-04 20:16:42 +0600

    error fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+// Use Node.js global __dirname directly
+
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   server: {
